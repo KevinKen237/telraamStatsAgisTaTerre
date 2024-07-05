1c1497c5e4a4a6c198f0b2f3490c939e8a24c1ea
KevinKen237 <kevinkenang1@gmail.com>
2024-07-05 17:06:50 +0200

    modify creation of data_speed in plot_speed

--- R/5_plot_speed.R
+++ R/5_plot_speed.R
@@ -25,8 +25,12 @@ plot_speed <- function(...){
 
   data_speed <- do.call(rbind,
                         lapply(data$car_speed_hist_0to70plus,
-                               function(i) {
-                                 speed <- unlist(i)
+                               function(i) {# i est une chaine de caractère
+                                 # Divisez la chaîne en valeurs individuelles
+                                 speed_str <- strsplit(i, ", ")[[1]]
+
+                                 # Convertissez en nombres
+                                 speed <- as.numeric(speed_str)
                                  speed_cumsum <- cumsum(speed)
                                  c(speed_cumsum[1], speed_cumsum[2], speed_cumsum[3], speed_cumsum[4])
                                }
@@ -75,7 +79,7 @@ plot_speed <- function(...){
   }
 
   chart <- ggplot(data_plot)+
-    aes(x=.data$count, y=.data$speed, color = .data$legend, group=.data$legend)+
+    aes(x=.data$count, y=.data$percentage, color = .data$legend, group=.data$legend)+
     geom_line(color="black")+ geom_smooth(method='gam', formula=y ~ s(x, bs = "cs"))+
     labs(x="Nombre de vehicules sur une tranche horaire", y = "Pourcentage de vehicule depassant la vitesse donnees")+
     ggtitle("Evolution de la vitesse de conduite selon le nombre d'usagers") +
